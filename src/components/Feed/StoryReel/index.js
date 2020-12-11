2518b88c909eb94891009bdf8a0bab9d2056ca90
Ammar Jussa <ammar26497@gmail.com>
2020-12-12 00:19:10 +0500

    Css updated

--- src/components/Feed/StoryReel/index.js
+++ src/components/Feed/StoryReel/index.js
@@ -50,6 +50,24 @@ function StoryReel({ user }) {
             : "https://scontent.fkhi4-1.fna.fbcdn.net/v/t1.0-1/p160x160/119741127_104766208049447_1835121773555063927_n.jpg?_nc_cat=103&_nc_sid=dbb9e7&_nc_eui2=AeGNDRiAVdxZ6yZ64m0SMEUarFAgzL5B8-esUCDMvkHz54bVZAZdzXaCbw14Qd6l1oaCrGFTcno1a8iHqraUtUQr&_nc_ohc=rkh3nuAOC_EAX9usbr3&_nc_ht=scontent.fkhi4-1.fna&tp=6&oh=45dfefb5eacdff75be1a54e1f82098ef&oe=5F9182D3"
         }
       />
+      <Story
+        image="https://scontent.fkhi4-2.fna.fbcdn.net/v/t1.0-9/43049815_1874400192649449_8704124977693065216_o.jpg?_nc_cat=109&ccb=2&_nc_sid=19026a&_nc_eui2=AeG24Yj2WOKQCpltdM2XniboT3JI02YUtJFPckjTZhS0kQqqSXMdxtjZkQFYnL7CS39SvohLV9hXPjY5aG_kBe2j&_nc_ohc=9-Xb1Uln8J8AX8DGa75&_nc_ht=scontent.fkhi4-2.fna&oh=71c677621869c7a5944a803deb0c0af7&oe=5FF58125"
+        title={user ? user.displayName : "Firebase User"}
+        profileSrc={
+          user
+            ? user.photoURL
+            : "https://scontent.fkhi4-1.fna.fbcdn.net/v/t1.0-1/p160x160/119741127_104766208049447_1835121773555063927_n.jpg?_nc_cat=103&_nc_sid=dbb9e7&_nc_eui2=AeGNDRiAVdxZ6yZ64m0SMEUarFAgzL5B8-esUCDMvkHz54bVZAZdzXaCbw14Qd6l1oaCrGFTcno1a8iHqraUtUQr&_nc_ohc=rkh3nuAOC_EAX9usbr3&_nc_ht=scontent.fkhi4-1.fna&tp=6&oh=45dfefb5eacdff75be1a54e1f82098ef&oe=5F9182D3"
+        }
+      />
+      <Story
+        image="https://scontent.fkhi4-2.fna.fbcdn.net/v/t1.0-9/43049815_1874400192649449_8704124977693065216_o.jpg?_nc_cat=109&ccb=2&_nc_sid=19026a&_nc_eui2=AeG24Yj2WOKQCpltdM2XniboT3JI02YUtJFPckjTZhS0kQqqSXMdxtjZkQFYnL7CS39SvohLV9hXPjY5aG_kBe2j&_nc_ohc=9-Xb1Uln8J8AX8DGa75&_nc_ht=scontent.fkhi4-2.fna&oh=71c677621869c7a5944a803deb0c0af7&oe=5FF58125"
+        title={user ? user.displayName : "Firebase User"}
+        profileSrc={
+          user
+            ? user.photoURL
+            : "https://scontent.fkhi4-1.fna.fbcdn.net/v/t1.0-1/p160x160/119741127_104766208049447_1835121773555063927_n.jpg?_nc_cat=103&_nc_sid=dbb9e7&_nc_eui2=AeGNDRiAVdxZ6yZ64m0SMEUarFAgzL5B8-esUCDMvkHz54bVZAZdzXaCbw14Qd6l1oaCrGFTcno1a8iHqraUtUQr&_nc_ohc=rkh3nuAOC_EAX9usbr3&_nc_ht=scontent.fkhi4-1.fna&tp=6&oh=45dfefb5eacdff75be1a54e1f82098ef&oe=5F9182D3"
+        }
+      />
     </div>
   );
 }
